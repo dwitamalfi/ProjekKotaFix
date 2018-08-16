d6fc6c2024d1da741aa3b65174a64c2a92d41c26
febrianpandu <pandapandu81@gmail.com>
2018-08-16 14:55:44 +0700

    Tautan
    Gambar perlu diperbaiki bosku

---
+++ application/views/Tautan.php
@@ -0,0 +1,62 @@
+<body>
+    <div class="main-content-wrapper section-padding-100">
+        <div class="container">
+            <!-- ============== Related Post ============== -->
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-4">
+                    <!-- Single Blog Post -->
+                    <div class="single-blog-post">
+                        <!-- Post Thumbnail -->
+                        <div class="post-thumbnail">
+                            <a href="https://www.kominfo.go.id/" target="_blank">
+                            <img src="<?php echo base_url();?>assets/img/core-img/kominfo-logo.jpg" alt="" >
+                        </div>
+                        <!-- Post Content -->
+                        <div class="post-content">
+                            <a href="https://www.kominfo.go.id/" class="headline">
+                                <h5>KEMENTRIAN KOMUNIKASI DAN INFORMATIKA</h5>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-6 col-lg-4">
+                    <!-- Single Blog Post -->
+                    <div class="single-blog-post">
+                        <!-- Post Thumbnail -->
+                        <div class="post-thumbnail">
+                            <a href="https://www.kemendagri.go.id/" target="_blank">
+                            <img src="<?php echo base_url();?>assets/img/core-img/kemendagri-logo.png" alt="">
+                        </div>
+                        <!-- Post Content -->
+                        <div class="post-content">
+                            <a href="https://www.kemendagri.go.id/" class="headline">
+                                <h5>KEMENTRIAN DALAM NEGERI</h5>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-6 col-lg-4">
+                    <!-- Single Blog Post -->
+                    <div class="single-blog-post">
+                        <!-- Post Thumbnail -->
+                        <div class="post-thumbnail">
+                            <a href="https://www.menpan.go.id/site/" target="_blank">
+                            <img src="<?php echo base_url();?>assets/img/core-img/kemenpan-logo.png" alt="">
+                        </div>
+                        <!-- Post Content -->
+                        <div class="post-content">
+                            <a href="https://www.menpan.go.id/site/" class="headline">
+                                <h5>KEMENTRIAN PENDAYAGUNAAN APARATUR NEGARA DAN REFORMASI BIROKRASI</h5>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+</body>
+
