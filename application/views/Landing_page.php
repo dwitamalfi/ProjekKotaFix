0de0da2ccdaf01ab45eda0744fd0622c1c7ff037
febrianpandu <pandapandu81@gmail.com>
2018-08-16 11:22:01 +0700

    age wes

--- application/views/Landing_page.php
+++ application/views/Landing_page.php
@@ -6,14 +6,12 @@
 	<meta name="keywords" content="kota Batu, Batu, pemerintah kota Batu, portal pemerintah kota Batu" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<meta name="author" content=""/>
-
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7CSource+Sans+Pro:400,700" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/landing-css/css/landing_page.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/landing-css/css/search.css">
 	<link rel=stylesheet type="text/css" href="<?php echo base_url();?>assets/landing-css/css/star-animation.css">
 	 <!-- <link rel=stylesheet type="text/css" href="assets/landing-css/css/bootstrap.css"> -->
-
 </head>
 
 <body>
@@ -65,158 +63,96 @@
 		<div class="col-md-12"> 
 			<div class="panel_menu wrap"> <!-- Menu atas -->
 				<div class="menu">
-					<div class="wrap_content" id="dropdown" data-id="atas"><img src="<?php echo base_url();?>assets/landing-css/img/icon/Profil-icon.png" width="" height=""></div>
-					<div class="wrap_content_active" id="dropdown" data-id="atas"><img src="<?php echo base_url();?>assets/landing-css/img/icon/Profil-icon.png" width="" height=""></div>
+					<div class="wrap_content" data-id="atas">
+						<a href='<?php echo base_url();?>index.php/Sejarah'>
+						<img src="<?php echo base_url();?>assets/landing-css/img/icon/Profil-icon.png" width="" height="">
+					</div>
 					<div class="font-wrap , warna">Profil Kota Batu</div>
-					<div class="arrow-up"></div>
+					<div class="arrow-up">
+					</div>
 				</div>
 				<div class="menu">
-					<div class="wrap_content" data-id="atas"><a href="" target="#banner"><img src="<?php echo base_url();?>assets/landing-css/img/icon/Pengumuman-icon.png" width="" height=""></a></div>
-					<div class="wrap_content_active" data-id="atas"><img src="<?php echo base_url();?>assets/landing-css/img/icon/Pengumuman-icon.png" width="" height=""></div>
+					<div class="wrap_content" data-id="atas">
+						<a href='<?php echo base_url();?>index.php/Pengumuman'>
+						<img src="<?php echo base_url();?>assets/landing-css/img/icon/Pengumuman-icon.png" width="" height=""></a></div>
+					<div class="wrap_content_active" data-id="atas">
+						<img src="<?php echo base_url();?>assets/landing-css/img/icon/Pengumuman-icon.png" width="" height=""></div>
 					<div class="font-wrap , warna">Pengumuman</div>
 					<!-- <div class="arrow-up"></div> -->
 				</div>
 				<div class="menu">
-					<div class="wrap_content" id="dropdown" data-id="atas"><img src="<?php echo base_url();?>assets/landing-css/img/icon/Agenda-icon.png" width="" height=""></div>
-					<div class="wrap_content_active" id="dropdown" data-id="atas"><img src="<?php echo base_url();?>assets/landing-css/img/icon/Agenda-icon.png" width="" height=""></div>
+					<div class="wrap_content" data-id="atas">
+						<a href='<?php echo base_url();?>index.php/Kalender'>
+						<img src="<?php echo base_url();?>assets/landing-css/img/icon/Agenda-icon.png" width="" height=""></div>
 					<div class="font-wrap , warna">Kalender Kota</div>
 					<div class="arrow-up"></div>
 				</div>
 				<div class="menu">
-					<div class="wrap_content" id="dropdown" data-id="atas"><img src="<?php echo base_url();?>assets/landing-css/img/icon/News-icon.png" width="" height=""></div>
-					<div class="wrap_content_active" id="dropdown" data-id="atas"><img src="<?php echo base_url();?>assets/landing-css/img/icon/News-icon.png" width="" height=""></div>
+					<div class="wrap_content" data-id="atas">
+						<a href='<?php echo base_url();?>index.php/Berita'>
+						<img src="<?php echo base_url();?>assets/landing-css/img/icon/News-icon.png" width="" height=""></div>
 					<div class="font-wrap , warna">Berita</div>
 					<div class="arrow-up"></div>
 				</div>
-				
-
 			</div>
 		</div>
 	</div>
 </div>
-
-
-	<div class="gelembung_informasi" id="atas"> 
-	</div> 
 	<div class="row">
 		<div class="col-md-12"> 
 			<div class="panel_menu wrap">
 				<div class="menu">
-					<div class="wrap_content"><a href="#"  target="_blank">
+					<div class="wrap_content">
+						<a href='<?php echo base_url();?>index.php/Pemerintahan'>
 						<img src="<?php echo base_url();?>assets/landing-css/img/icon/Pemerintahan-icon.png" width="" height=""></a></div>
 					<div class="font-wrap"><a href="#"  target="_blank">Pemerintahan</a></div>
 				</div>
 				<div class="menu">
-					<div class="wrap_content"><a href="http://jdih.batukota.go.id"  target="_blank"><img src="<?php echo base_url();?>assets/landing-css/img/icon/JDIH-icon.png" width="" height=""></a></div>
-					<div class="font-wrap"><a href="http://jdih.batukota.go.id"  target="_blank">JDIH</a></div>
+					<div class="wrap_content">
+						<a href="http://jdih.batukota.go.id"  target="_blank">
+							<img src="<?php echo base_url();?>assets/landing-css/img/icon/JDIH-icon.png" width="" height=""></a></div>
+					<div class="font-wrap">
+						<a href="http://jdih.batukota.go.id"  target="_blank">JDIH</a></div>
 				</div>
 				<div class="menu">
-					<div class="wrap_content"><a href="http://ppid.batukota.go.id"  target="_blank"><img src="<?php echo base_url();?>assets/landing-css/img/icon/PPID-icon.png" width="" height=""></a></div>
+					<div class="wrap_content">
+						<a href="http://ppid.batukota.go.id"  target="_blank">
+							<img src="<?php echo base_url();?>assets/landing-css/img/icon/PPID-icon.png" width="" height=""></a></div>
 					<div class="font-wrap"><a href="http://ppid.batukota.go.id"  target="_blank">PPID</a></div>
 				</div>
 				<div class="menu">
-					<div class="wrap_content" id="dropdown" data-id="bawah"><a href="http://LPSE.batukota.go.id"><img src="<?php echo base_url();?>assets/landing-css/img/icon/LPSE-icon.png" width="" height=""></div>
+					<div class="wrap_content" >
+						<a href="http://LPSE.batukota.go.id" target="_blank">
+							<img src="<?php echo base_url();?>assets/landing-css/img/icon/LPSE-icon.png" width="" height=""></div>
 					<div class="wrap_content_active"><img src="<?php echo base_url();?>assets/landing-css/img/icon/LPSE-icon.png" width="" height=""></div>
 					<div class="font-wrap , warna">LPSE</div>
-					<div class="arrow-up"></div>
 				</div>
 				<div class="menu">
-					<div class="wrap_content"><a href="http://simata.kotabogor.go.id"  target="_blank"><img src="<?php echo base_url();?>assets/landing-css/img/icon/Tautan-icon.png" width="" height=""></a></div>
+					<div class="wrap_content">
+						<a href='<?php echo base_url();?>index.php/Tautan'>
+						<img src="<?php echo base_url();?>assets/landing-css/img/icon/Tautan-icon.png" width="" height=""></a></div>
 					<div class="font-wrap"><a href="http://simata.kotabogor.go.id"  target="_blank">Tautan</a></div>
 				</div>
 			</div>
-			<div class="gelembung_informasi" id="bawah">
-			</div>
 		</div>
 	</div>
-	
-	
 	<div class="row">
 			<div class="col-md-12">
 				<div class="contact">
 					<div class="sosmed">
-						<a href="https://www.facebook.com/DiskominfoKWB/"><img class="icon" src="<?php echo base_url();?>assets/landing-css/img/icon/facebook-icon.png"><b>FACEBOOK</b><br>@DiskominfoKWB</a>
+						<a href="https://www.facebook.com/DiskominfoKWB/" target="_blank"><img class="icon" src="<?php echo base_url();?>assets/landing-css/img/icon/facebook-icon.png"><b>FACEBOOK</b><br>@DiskominfoKWB</a>
 					</div>
 					<div class="sosmed">
-						<a href="https://www.twitter.com/kominfobatukota/"><img class="icon" src="<?php echo base_url();?>assets/landing-css/img/icon/twitter-icon.png"><b>TWITTER</b><br>@kominfobatukota</a>
+						<a href="https://www.twitter.com/kominfobatukota/" target="_blank"><img class="icon" src="<?php echo base_url();?>assets/landing-css/img/icon/twitter-icon.png"><b>TWITTER</b><br>@kominfobatukota</a>
 					</div>
 					<div class="sosmed">
-						<a href="https://www.instagram.com/DiskominfoKWB/"><img class="icon" src="<?php echo base_url();?>assets/landing-css/img/icon/instagram-icon.png"><b>INSTAGRAM</b><br>@Diskominfo_KWB</a>
+						<a href="https://www.instagram.com/DiskominfoKWB/" target="_blank"><img class="icon" src="<?php echo base_url();?>assets/landing-css/img/icon/instagram-icon.png"><b>INSTAGRAM</b><br>@Diskominfo_KWB</a>
 					</div>
 					<div class="sosmed">
-						<a href="https://www.youtube.com/channel/UCwTFu67EcRtN4010RmJyUbg"><img class="icon" src="<?php echo base_url();?>assets/landing-css/img/icon/youtube-icon.png"><b>YOUTUBE</b><br>Diskominfo Kota Batu</a>
+						<a href="https://www.youtube.com/channel/UCwTFu67EcRtN4010RmJyUbg" target="_blank"><img class="icon" src="<?php echo base_url();?>assets/landing-css/img/icon/youtube-icon.png"><b>YOUTUBE</b><br>Diskominfo Kota Batu</a>
 					</div>
 				</div>
 			</div>
 		</div>
-	
-	
 </body>
 </html>
-
-<script type="text/javascript">
-$(document).ready(function(){
-	$(".wrap_content#dropdown").click(function(){
-		var bagian = $(this).data("id");
-		$(".wrap_content_active").hide("slow","linear");
-		$(".arrow-up").slideUp("slow","linear");
-		$(".wrap_content").show("slow","linear");
-		$(".gelembung_informasi#atas").slideUp("slow","linear");
-		$(".gelembung_informasi#bawah").slideUp("slow","linear");
-			$(this).parent().children(".wrap_content_active").slideToggle("slow","linear");
-			$(".wrap_content_active").attr("Id","selected");
-			$(this).toggle(250,"linear");
-			$(".gelembung_informasi#"+bagian).slideDown("slow","linear");
-			//$(this).parent().children(".arrow-up").toggle("slow","linear"); yang asli
-			$(this).parent().children(".arrow-up").slideDown("slow","linear");
-			$("body").css("overflow-y","scroll");
-			$("body").css("background-size","100% 150%");
-		var menu 	=  	$(this).parent().children(".font-wrap").html();
-		var masukan;
-		switch (menu){
-			case "Profil Kota Batu":
-				masukan = "<div class='listing'> <a href='<?php echo base_url();?>index.php/Sejarah'> Sejarah Kota Batu</a><br><br><a href='http://dispenda.kotabogor.go.id'> Lambang Kota Batu</a><br><br><a href='http://perizinan.kotabogor.go.id'> Pimpinan Daerah</a></div><div class='listing'><a href='http://disnakersostrans.kotabogor.go.id'> Peta</a>";
-				//masukan = menu;
-			break;
-			// case "Pengumuman":
-			// 	masukan = "<div class='listing'><a>Masih Belum ada</a></div>";
-			// break;
-			case "Sekilas Kota Batu":
-				masukan = "<div class='listing'><a href='http://kotabogor.go.id/index.php/page/detail/5/sejarah-bogor'><i class='fa fa-long-arrow-right'></i> Sejarah Kota Bogor</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/8/lambang-kota-bogor'><i class='fa fa-long-arrow-right'></i> Lambang Kota Bogor</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/9/letak-geografis'><i class='fa fa-long-arrow-right'></i> Letak Geografis</a></div><div class='listing'><a href='http://simpatik.kotabogor.go.id/'><i class='fa fa-long-arrow-right'></i> Data Demografi</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/78/peta-kota-bogor'><i class='fa fa-long-arrow-right'></i> Peta Kota Bogor</a><br><br><a href='http://kotabogor.go.id/index.php/gallery/detailalbum/1'><i class='fa fa-long-arrow-right'></i> Bogor Tempo Dulu</a></div>";
-			break;
-			case "Pemerintahan":
-				masukan = "<div class='listing'><a href='http://kotabogor.go.id/index.php/page/detail/54/pimpinan-daerah'><i class='fa fa-long-arrow-right'></i> Pimpinan Daerah</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/39/data-muspimwil-dan-muspida-kota-bogor'><i class='fa fa-long-arrow-right'></i> Muspimwil / Muspidu</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/1/struktur-organisasi'><i class='fa fa-long-arrow-right'></i> Struktur Pemerintahan</a></div><div class='listing'><a href='http://kotabogor.go.id/index.php/page/detail/6/rencana-kerja-kota-bogor'><i class='fa fa-long-arrow-right'></i> Dokumen Perencanaan</a><br><br><a href='#'><i class='fa fa-long-arrow-right'></i> Subdomain / Aplikasi</a><br><br><a href='http://siskum.kotabogor.go.id/'><i class='fa fa-long-arrow-right'></i> Perundang - Undangan</a></div><div class='listing'><a href='http://kotabogor.go.id/index.php/page/detail/52/opd'><i class='fa fa-long-arrow-right'></i> Perangkat Daerah</a><br><br>	<a href='http://kotabogor.go.id/index.php/page/detail/62/bumd'><i class='fa fa-long-arrow-right'></i> BUMD</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/119/prestasi-dan-penghargaan'><i class='fa fa-long-arrow-right'></i> Prestasi Penghargaan</a></div><div class='listing'><a href='http://kotabogor.go.id/index.php/page/detail/81/kerjasama-pemerintah-daerah'><i class='fa fa-long-arrow-right'></i> Kerjasama Pemerintah Daerah</a><br><br></div>";
-			break;
-			case "Investasi":
-				masukan = "<div class='listing'><a href='http://bpptpm.kotabogor.go.id/index.php/multisite/layanankategori/36'><i class='fa fa-long-arrow-right'></i> Izin Bidang Pelayanan Modal</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/120/potensi-usaha'><i class='fa fa-long-arrow-right'></i> Potensi Usaha</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/121/daerah-pengembangan-usaha'><i class='fa fa-long-arrow-right'></i> Daerah Pengembangan Usaha</a></div><div class='listing'><a href='http://kotabogor.go.id/index.php/page/detail/122/perencanaan-usaha'><i class='fa fa-long-arrow-right'></i> Perencanaan Usaha</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/23/infrastruktur'><i class='fa fa-long-arrow-right'></i> Infrastruktur</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/17/produk-unggulan'><i class='fa fa-long-arrow-right'></i> Produk Unggulan</a></div><div class='listing'><a href='http://kotabogor.go.id/index.php/page/detail/124/industri'><i class='fa fa-long-arrow-right'></i> Industri</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/125/ariwisata'><i class='fa fa-long-arrow-right'></i> Pariwisata</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/20/pendidikan'><i class='fa fa-long-arrow-right'></i> Pendidikan</a></div><div class='listing'><a href='http://kotabogor.go.id/index.php/page/detail/24/ekspor'><i class='fa fa-long-arrow-right'></i> Ekspor</a><br><br><a href='http://kumkm.kotabogor.go.id'><i class='fa fa-long-arrow-right'></i> Daftar UKM</a><br><br></div>";
-			break;
-			case "Transparansi":
-				masukan = "<div class='listing'><a href='http://kotabogor.go.id/index.php/page/detail/299/ringkasan-rka-skpd'><i class='fa fa-long-arrow-right'></i> Ringkasan RKA SKPD</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/102/ringkasan-rka-ppkd'><i class='fa fa-long-arrow-right'></i> Ringkasan RKA PPKD</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/80/perda-tentang-apbd%20'><i class='fa fa-long-arrow-right'></i> Perda Tentang APBD</a></div><div class='listing'><a href='http://kotabogor.go.id/index.php/page/detail/258/perwali-tentang-apbd-'><i class='fa fa-long-arrow-right'></i> Perwali Tentang APBD</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/113/raperda-tentang-apbd-02'><i class='fa fa-long-arrow-right'></i> RAPERDA Tentang APBD</a><br><br><a href='#'><i lass='fa fa-long-arrow-right'></i> RAPERDA Tentang Perubahan APBD</a></div><div class='listing'><a href='http://kotabogor.go.id/index.php/page/detail/280/ringkasan-dpa-skpd-tahun-2015'><i class='fa fa-long-arrow-right'></i> Ringkasan DPA SKPD</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/110/ringkasan-dpa-ppkd'><i class='fa fa-long-arrow-right'></i> Ringkasan DPA PPKD</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/104/lra-seluruh-skpd-2014'><i class='fa fa-long-arrow-right'></i> LRA Seluruh SKPD</a></div><div class='listing'><a href='#'><i class='fa fa-long-arrow-right'></i> LRA PPKD</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/107/lkpd-sudah-teraudit#.WcHlcfOg_IU'><i class='fa fa-long-arrow-right'></i> LKPD Sudah Teraudit</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/282/opini-atas-lkpd'><i class='fa fa-long-arrow-right'></i> Laporan Keterbukaan Informasi Publik</a></div><div class='listing'><a href='http://kotabogor.go.id/index.php/page/detail/382/ppid-kota-bogor'><i class='fa fa-long-arrow-right'></i> PPID Kota Bogor</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/400/laporan-kinerja-instansi-pemerintah'><i class='fa fa-long-arrow-right'></i> Laporan Kinerja Instansi Pemerintah</a><br><br><a href='http://kotabogor.go.id/index.php/page/detail/401/indikator-kinerja-utama'><i class='fa fa-long-arrow-right'></i> Indikator Kinerja Utama</a></div>";
-			break;
-			case "Pariwisata":
-				masukan = "<div class='listing'>@:<a href='http://direktori.kotabogor.go.id/index.php/situs/wisatahot'><i class='fa fa-long-arrow-right'></i> Hotel</a><br><br><a href='http://direktori.kotabogor.go.id/index.php/situs/wisatakul'><i class='fa fa-long-arrow-right'></i> Rumah Makan dan Restoran</a><br><br><a href='http://direktori.kotabogor.go.id/index.php/situs/wisatatrav'><i class='fa fa-long-arrow-right'></i> Tour dan Travel</a></div><div class='listing'><a href='#'><i class='fa fa-long-arrow-right'></i> Money Changer</a><br><br><a href='http://direktori.kotabogor.go.id/index.php/situs/wisatabel'><i class='fa fa-long-arrow-right'></i> Pusat Perbelanjaan</a><br><br><a href='http://direktori.kotabogor.go.id/index.php/situs/wisatahib'><i class='fa fa-long-arrow-right'></i> Tempat Rekreasi</a></div><div class='listing'><a href='#'><i class='fa fa-long-arrow-right'></i> Jajanan Oleh - Oleh</a><br><br><a href='http://direktori.kotabogor.go.id/index.php/situs/wisatacend'><i class='fa fa-long-arrow-right'></i> Kerajinan dan Sovenir</a></div>";
-			break;
-		}
-
-		$(".gelembung_informasi#"+bagian).html(masukan);
-	});
-	$(".wrap_content_active").click(function(){
-		$(this).parent().children(".wrap_content").slideToggle("slow","linear");
-		$(this).toggle(250,"linear");
-		$(".gelembung_informasi#bawah").slideUp("slow","linear");
-		$(".gelembung_informasi#atas").slideUp("slow","linear");
-		$(this).parent().children(".arrow-up").slideUp("slow","linear");
-		$("body").css("overflow-y","hidden");
-		$("body").css("background-size","100% 100%");
-	});
-	 var w = window,
-		     d = document,
-		     e = d.documentElement,
-	  	     g = d.body,
-		     x = w.innerWidth || e.clientWidth || g.clientWidth, // x = Lebar layar
-		     y = w.innerHeight || e.clientHeight || g.clientHeight; // y = Tinggi layar
-
-		// // Tampilkan lebar dan tinggi layar dalam kotak pesan
-		 //alert('Lebar layar: ' + x + '; Tinggi layar: ' + y);
-});
-</script>
