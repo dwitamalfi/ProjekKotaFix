ac1b36ffd028f1c94e45628b31b1b11c5aad0a92
Bagus Cahyo P <baguscahyocahyo@gmail.com>
2018-08-24 05:04:21 +0700

    berita

---
+++ application/views/Profil/Pimpinan.php
@@ -0,0 +1,22 @@
+<body>
+    <!-- ***** Header Area Start ***** -->
+
+  
+    <!-- ***** Header Area End ***** -->
+
+    <!-- ********** Hero Area Start ********** -->
+   
+    <!-- ********** Hero Area End ********** -->
+
+    <div class="main-content-wrapper section-padding-100">
+      
+    </div>
+
+    <!-- ***** Footer Area Start ***** -->
+ 
+    <!-- ***** Footer Area End ***** -->
+
+    <!-- jQuery (Necessary for All JavaScript Plugins) -->
+    
+</body>
+
