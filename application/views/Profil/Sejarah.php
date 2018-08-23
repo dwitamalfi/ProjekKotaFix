f75fe69863c6de06737fd3ba8aba3b392de644b4
febrianpandu <pandapandu81@gmail.com>
2018-08-23 13:30:37 +0700

    baru

--- application/views/Profil/Sejarah.php
+++ application/views/Profil/Sejarah.php
@@ -1,4 +1,4 @@
-<body>
+<body style="background-color: #E1F1E3">
     <!-- ***** Header Area Start ***** -->
 
   
@@ -8,17 +8,17 @@
    
     <!-- ********** Hero Area End ********** -->
 
-    <div class="main-content-wrapper section-padding-100">
+    <div class="main-content-wrapper section-padding-100" >
         <div class="container">
             <div class="row justify-content-center">
                 <!-- ============= Post Content Area ============= -->
-                <div class="col-12 col-lg-8">
+                <div class="col-12 col-lg-8" >
                     <div class="single-blog-content mb-100">
                         <!-- Post Meta -->
                         
                         <!-- Post Content -->
                         <h5 class="title">Sejarah Kota Batu</h5>
-                        <div class="post-content">
+                        <div class="post-content" style="background-color: white">
                            
                             <h6>Sejak abad ke-10, wilayah Batu dan sekitarnya telah dikenal sebagai tempat peristirahatan bagi kalangan keluarga kerajaan, karena wilayah adalah daerah pegunungan dengan kesejukan udara yang nyaman, juga didukung oleh keindahan pemandangan alam sebagai ciri khas daerah pegunungan. Pada waktu pemerintahan Raja Sindok , seorang petinggi Kerajaan bernama Mpu Supo diperintah Raja Sendok untuk membangun tempat peristirahatan keluarga kerajaan di pegunungan yang didekatnya terdapat mata air. Dengan upaya yang keras, akhirnya Mpu Supo menemukan suatu kawasan yang sekarang lebih dikenal sebagai kawasan Wisata Songgoriti. Atas persetujuan Raja, Mpu Supo yang konon kabarnya juga sakti mandraguna itu mulai membangun kawasan Songgoriti sebagai tempat peristirahatan keluarga kerajaan serta dibangunnya sebuah candi yang diberi nama Candi Supo. Sebagaimana keinginan Raja, ditempat peristirahatan tersebut terdapat sumber mata air yang mengalir dingin dan sejuk seperti semua mata air di wilayah pegunungan. Mata air dingin tersebut sering digunakan mencuci keris-keris yang bertuah sebagai benda pusaka dari kerajaan Sendok. Oleh karena sumber mata air yang sering digunakan untuk mencuci benda-benda kerajaan yang bertuah dan mempunyai kekuatan supranatural (Magic) yang maha dasyat, akhirnya sumber mata air yang semula terasa dingin dan sejuk akhirnya berubah menjadi sumber air panas. Dan sumber air panas itupun sampai saat ini menjadi sumber abadi di kawasan Wisata Songgoriti. </h6>
                             <!-- <blockquote class="mb-30">
@@ -42,80 +42,7 @@
                 
             </div>
 
-            <!-- ============== Related Post ============== -->
-            <div class="row">
-                <div class="col-12 col-md-6 col-lg-4">
-                    <!-- Single Blog Post -->
-                    <div class="single-blog-post">
-                        <!-- Post Thumbnail -->
-                        <div class="post-thumbnail">
-                            <img src="<?php echo base_url();?>assets/img/blog-img/b1.jpg" alt="">
-                            <!-- Catagory -->
-                            <div class="post-cta"><a href="#">travel</a></div>
-                        </div>
-                        <!-- Post Content -->
-                        <div class="post-content">
-                            <a href="#" class="headline">
-                                <h5>How Did van Gogh’s Turbulent Mind Depict One of the Most Complex Concepts in Physics?</h5>
-                            </a>
-                            <p>How Did van Gogh’s Turbulent Mind Depict One of the Most Complex Concepts in...</p>
-                            <!-- Post Meta -->
-                            <div class="post-meta">
-                                <p><a href="#" class="post-author">Katy Liu</a> on <a href="#" class="post-date">Sep 29, 2017 at 9:48 am</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-12 col-md-6 col-lg-4">
-                    <!-- Single Blog Post -->
-                    <div class="single-blog-post">
-                        <!-- Post Thumbnail -->
-                        <div class="post-thumbnail">
-                            <img src="<?php echo base_url();?>assets/img/blog-img/b2.jpg" alt="">
-                            <!-- Catagory -->
-                            <div class="post-cta"><a href="#">travel</a></div>
-                        </div>
-                        <!-- Post Content -->
-                        <div class="post-content">
-                            <a href="#" class="headline">
-                                <h5>How Did van Gogh’s Turbulent Mind Depict One of the Most Complex Concepts in Physics?</h5>
-                            </a>
-                            <p>How Did van Gogh’s Turbulent Mind Depict One of the Most Complex Concepts in...</p>
-                            <!-- Post Meta -->
-                            <div class="post-meta">
-                                <p><a href="#" class="post-author">Katy Liu</a> on <a href="#" class="post-date">Sep 29, 2017 at 9:48 am</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-12 col-md-6 col-lg-4">
-                    <!-- Single Blog Post -->
-                    <div class="single-blog-post">
-                        <!-- Post Thumbnail -->
-                        <div class="post-thumbnail">
-                            <img src="<?php echo base_url();?>assets/img/blog-img/b7.jpg" alt="">
-                            <!-- Catagory -->
-                            <div class="post-cta"><a href="#">travel</a></div>
-                            <!-- Video Button -->
-                            <a href="https://www.youtube.com/watch?v=IhnqEwFSJRg" class="video-btn"><i class="fa fa-play"></i></a>
-                        </div>
-                        <!-- Post Content -->
-                        <div class="post-content">
-                            <a href="#" class="headline">
-                                <h5>How Did van Gogh’s Turbulent Mind Depict One of the Most Complex Concepts in Physics?</h5>
-                            </a>
-                            <p>How Did van Gogh’s Turbulent Mind Depict One of the Most Complex Concepts in...</p>
-                            <!-- Post Meta -->
-                            <div class="post-meta">
-                                <p><a href="#" class="post-author">Katy Liu</a> on <a href="#" class="post-date">Sep 29, 2017 at 9:48 am</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+            
             
         </div>
     </div>
