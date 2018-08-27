d480afa7691b418ffcf2d7bcc5eb688589b1c40d
febrianpandu <pandapandu81@gmail.com>
2018-08-27 21:27:03 +0700

    perapian

--- application/views/Profil/Sejarah.php
+++ application/views/Profil/Sejarah.php
@@ -20,20 +20,23 @@
                         <h5 class="title">Sejarah Kota Batu</h5>
                         <div class="post-content" >
                            
-                            <h6 align="left|right|center|justify">Sejak abad ke-10, wilayah Batu dan sekitarnya telah dikenal sebagai tempat peristirahatan bagi kalangan keluarga kerajaan, karena wilayah adalah daerah pegunungan dengan kesejukan udara yang nyaman, juga didukung oleh keindahan pemandangan alam sebagai ciri khas daerah pegunungan. Pada waktu pemerintahan Raja Sindok , seorang petinggi Kerajaan bernama Mpu Supo diperintah Raja Sendok untuk membangun tempat peristirahatan keluarga kerajaan di pegunungan yang didekatnya terdapat mata air. Dengan upaya yang keras, akhirnya Mpu Supo menemukan suatu kawasan yang sekarang lebih dikenal sebagai kawasan Wisata Songgoriti. Atas persetujuan Raja, Mpu Supo yang konon kabarnya juga sakti mandraguna itu mulai membangun kawasan Songgoriti sebagai tempat peristirahatan keluarga kerajaan serta dibangunnya sebuah candi yang diberi nama Candi Supo. Sebagaimana keinginan Raja, ditempat peristirahatan tersebut terdapat sumber mata air yang mengalir dingin dan sejuk seperti semua mata air di wilayah pegunungan. Mata air dingin tersebut sering digunakan mencuci keris-keris yang bertuah sebagai benda pusaka dari kerajaan Sendok. Oleh karena sumber mata air yang sering digunakan untuk mencuci benda-benda kerajaan yang bertuah dan mempunyai kekuatan supranatural (Magic) yang maha dasyat, akhirnya sumber mata air yang semula terasa dingin dan sejuk akhirnya berubah menjadi sumber air panas. Dan sumber air panas itupun sampai saat ini menjadi sumber abadi di kawasan Wisata Songgoriti. </h6>
-                            <!-- <blockquote class="mb-30">
-                                <h6>Aliquam auctor lacus a dapibus pulvinar. Morbi in elit erat. Quisque et augue nec tortor blandit hendrerit eget sit amet sapien. Curabitur at tincidunt metus, quis porta ex. Duis lacinia metus vel eros cursus pretium eget.</h6>
-                                <div class="post-author">
-                                    <p>Robert Morgan</p>
-                                </div>
-                            </blockquote> -->
-                            
-                            <!-- Post Tags -->
-                            
-                            <!-- Post Meta -->
-                            <div class="post-meta second-part">
-                                <p><a href="#" class="post-author">Katy Liu</a> on <a href="#" class="post-date">Sep 29, 2017 at 9:48 am</a></p>
-                            </div>
+                            <h6 align="justify">Sejak abad ke-10, wilayah Batu dan sekitarnya telah dikenal sebagai tempat peristirahatan bagi kalangan keluarga kerajaan, karena wilayah adalah daerah pegunungan dengan kesejukan udara yang nyaman, juga didukung oleh keindahan pemandangan alam sebagai ciri khas daerah pegunungan. </h6>
+                           <h6 align="justify"> Pada waktu pemerintahan Raja Sindok , seorang petinggi Kerajaan bernama Mpu Supo diperintah Raja Sendok untuk membangun tempat peristirahatan keluarga kerajaan di pegunungan yang didekatnya terdapat mata air. Dengan upaya yang keras, akhirnya Mpu Supo menemukan suatu kawasan yang sekarang lebih dikenal sebagai kawasan Wisata Songgoriti. </h6>
+                            <h6 align="justify"> Atas persetujuan Raja, Mpu Supo yang konon kabarnya juga sakti mandraguna itu mulai membangun kawasan Songgoriti sebagai tempat peristirahatan keluarga kerajaan serta dibangunnya sebuah candi yang diberi nama Candi Supo. Sebagaimana keinginan Raja, ditempat peristirahatan tersebut terdapat sumber mata air yang mengalir dingin dan sejuk seperti semua mata air di wilayah pegunungan. </h6>
+
+                            <h6 align="justify">Mata air dingin tersebut sering digunakan mencuci keris-keris yang bertuah sebagai benda pusaka dari kerajaan Sendok. Oleh karena sumber mata air yang sering digunakan untuk mencuci benda-benda kerajaan yang bertuah dan mempunyai kekuatan supranatural (Magic) yang maha dasyat, akhirnya sumber mata air yang semula terasa dingin dan sejuk akhirnya berubah menjadi sumber air panas. Dan sumber air panas itupun sampai saat ini menjadi sumber abadi di kawasan Wisata Songgoriti. </h6>
+
+                            <h6 align="justify">Wilayah Kota Batu yang terletak di dataran tinggi di lereng pegunungan dengan ketinggian 700 sampai 1.700 meter di atas permukaan laut, berdasarkan kisah-kisah orang tua maupun dokumen yang ada maupun yang dilacak keberadaannya, sampai saat ini belum diketahui kepastiannya tentang kapan nama “Batu” mulai disebut untuk menamai kawasan peristirahatan tersebut.</h6>
+
+                          <h6 align="justify">Dari beberapa pemuka masyarakat setempat memang pernah mengisahkan bahwa sebutan Batu berasal dari nama seorang ulama pengikut Pangeran Diponegoro yang bernama Abu Ghonaim atau disebut sebagai Kyai Gubug Angin yang selanjutnya masyarakat setempat akrab menyebutnya dengan panggilan Mbah Wastu.</h6>
+
+                          <h6 align="justify">Dari kebiasaan kultur Jawa yang sering memperpendek dan mempersingkat mengenai sebutan nama seseorang yang dirasa terlalu panjang, juga agar lebih singkat penyebutannya serta lebih cepat bila memanggil seseorang, akhirnya lambat laun sebutan Mbah Wastu dipanggil Mbah Tu menjadi Mbatu atau Batu sebagai sebutan yang digunakan untuk sebuah kota dingin di Jawa Timur.</h6>
+
+                          <h6 align="justify">Kota Batu dahulu merupakan bagian dari Kabupaten Malang, yang kemudian ditetapkan menjadi kota administratif pada 6 Maret 1993. Pada tanggal 17 Oktober 2001, Batu ditetapkan sebagai kota otonom yang terpisah dari Kabupaten Malang.</h6>
+                        <hr>
+                            <!-- <div class="post-meta second-part"> -->
+                                <!-- <p><a href="#" class="post-author">Sumber : </a> on <a href="#" class="post-date">Sep 29, 2017 at 9:48 am</a></p> -->
+                            <!-- </div> -->
                         </div>
                     </div>
                 </div>
